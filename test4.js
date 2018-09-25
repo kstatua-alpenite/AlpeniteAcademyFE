e1959178b9aec58ab17114ae762faf40548e35b9
kstatua-alpenite <37293710+kstatua-alpenite@users.noreply.github.com>
2018-09-25 15:10:20 +0200

    Gioco2.js v2

--- test4.js
+++ test4.js
@@ -4,10 +4,23 @@
  * Descrizione: Creazione di un gioco di dadi
  * Regole: 
  *    STEP 1: 
- *          Giocano 2 player e girano un dado uno alla volta quando vogliono.
+ *          Giocano 2 player e tirano un dado uno alla volta quando vogliono.
  *    STEP 2: 
  *          Chi gira il dado con il valore più alto vince
  *    STEP 3:
  *          Se il vincitore vince due volte di fila allora ha perso
  *
  */
+
+var vittoriaConsecutiva = 0,
+    tiroA = 0,
+    tiroB = 0;
+
+// <button onclick="nomeFunzione()"></button>
+/*
+  <script type="text/javascript">
+    function nomeFunzione() {
+      console.log("lanciata!!!");
+    }
+  </script>
+*/
