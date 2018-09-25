7a40271439ac05489e701c509fe16be91d8f061c
kstatua-alpenite <37293710+kstatua-alpenite@users.noreply.github.com>
2018-09-25 17:38:26 +0200

    Shop1.js

---
+++ test5.js
@@ -0,0 +1,35 @@
+/**
+
+Visibilità prodotto : 
+
+    isActive                false   true        true
+    isInStock               true    true        false
+    isWarehouseClose        true    true        true
+
+Dati prodotto:
+
+    name                    Pizza   Piadina     Kebab
+    price                   8       5           4.5
+    qty                     10      30          20
+
+Dati generici: 
+
+    taxRate                 21      21          21
+    shippingPrice           1       0.5         0.5
+
+
+Tre box a frontend:
+
+
+____________________|____________________|____________________|
+IMMAGINE DELLA      |IMMAGINE DELLA      |IMMAGINE DEL        |
+PIZZA               |PIADINA             |KEBAB               |
+                    |                    |                    |
+quantità della      |quantità della      |quantità del        |
+Pizza               |Piadina             |Kebab               |
+                    |                    |                    |
+Button acquista     |Button acquista     |Button acquista     |
+                    |                    |                    |
+____________________|____________________|____________________|
+
+*/
