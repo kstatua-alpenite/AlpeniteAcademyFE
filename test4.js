7083b82a3f6468df9070e5679e374fb0b1d330dd
kstatua-alpenite <37293710+kstatua-alpenite@users.noreply.github.com>
2018-09-25 14:53:03 +0200

    Gioco2.js

---
+++ test4.js
@@ -0,0 +1,13 @@
+/* creazione minigioco */
+
+/* Titolo: Dadi
+ * Descrizione: Creazione di un gioco di dadi
+ * Regole: 
+ *    STEP 1: 
+ *          Giocano 2 player e girano un dado uno alla volta quando vogliono.
+ *    STEP 2: 
+ *          Chi gira il dado con il valore più alto vince
+ *    STEP 3:
+ *          Se il vincitore vince due volte di fila allora ha perso
+ *
+ */
